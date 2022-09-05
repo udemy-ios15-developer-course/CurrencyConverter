03b49bd4d9bb386ec558409890513a1c9adbee4e
Chris Hand <cj.hand@gmail.com>
2022-09-04 18:33:09 -0700

    Building out the request

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -22,7 +22,21 @@ class ViewController: UIViewController {
 
     @IBAction func getRatesClicked(_ sender: Any) {
         
-        
+        let url = URL(string:"http://data.fixer.io/api/latest?access_key=4nRlNOZXvTyTVIzmfDKAX6wkaxwGU9c0")!
+        let session = URLSession.shared
+        let task = session.dataTask(with: url) { data, response, error in
+            if error == nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                let button = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+                alert.addAction(button)
+                self.present(alert, animated: true)
+                return
+            }
+            
+            if data == nil {
+                return
+            }
+        }
     }
 }
 
